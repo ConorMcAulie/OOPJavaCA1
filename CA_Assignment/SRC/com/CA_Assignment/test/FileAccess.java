eb90ca89ae4e909a5599906f013413ae2b04eb3d
Conor McAuley <conormcauley01@gmail.com>
2019-04-11 20:54:51 +0100

    coding at home

---
+++ CA_Assignment/SRC/com/CA_Assignment/test/FileAccess.java
@@ -0,0 +1,65 @@
+package com.CA_Assignment.test;
+
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.PrintWriter;
+import java.util.ArrayList;
+import java.util.Scanner;
+
+public class FileAccess 
+{
+	int fileSize;
+	String name;
+	File dataSet;
+	Scanner dataSetScanner;
+	PrintWriter fileApend;
+	ArrayList<String[]> dataSetArr = new ArrayList<String[]>();
+	
+	FileAccess(String name)
+	{
+		this.name = name;
+		dataSet = new File(name);
+	}
+	
+	public String[] readfile()
+	{
+		try 
+		{
+			dataSetScanner = new Scanner(dataSet);
+		} 
+		catch (FileNotFoundException e) 
+		{
+			System.out.println("run time error " + e.getMessage());
+			String[] empty = new String[1];
+  			return empty;
+		}
+		String[] readFile = new String[(int) dataSet.length()];
+		return readFile;
+	}
+	
+	public void addPatientToData(Patient patTemp) 
+	{
+		int getPatSize = 2;
+		try
+    	{
+    		fileApend = new PrintWriter(dataSet);
+    	}
+  		catch (FileNotFoundException e)
+  		{
+  			System.out.println("run time error " + e.getMessage());
+  			return;
+  		}
+		for(int i = 0; i < getPatSize; i++) 
+		{
+			
+		}
+		fileApend.println(patTemp);
+		fileApend.close();
+	}
+	
+	public void resetData()
+	{
+		File trainingData = new File("TrainingData");
+		dataSet = trainingData;
+	}
+}
--- CA_Assignment/SRC/com/CA_Assignment/test/Patient.java
+++ CA_Assignment/SRC/com/CA_Assignment/test/Patient.java
@@ -1,30 +1,13 @@
-package com.CA_Assignment.test;
+             package com.CA_Assignment.test;
 
 @SuppressWarnings("unused")
 public class Patient {
 	
 	private String[] Traits;
+	private int patSize;
 	
 	public Patient()
 	{
 		
 	}
-	/*
-	public void printSet()
-	{
-		Iterator<String> itHS = testSet.iterator();
-		while(itHS.hasNext())
-		{
-			System.out.println(itHS.next());
-		}
-		System.out.println("\n**********\n");
-	}
-	*/
-	
-	/*
-	public void clearSet()
-	{
-		testSet.clear();
-	}
-	*/
 }
